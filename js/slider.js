665f8d17789bea678ffd681f4caefa87668ce860
Kondi <Kondi.171@wp.pl>
2020-10-23 20:31:12 +0200

    finish JS scripts

--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const slideList = [{
-        desktop: "../img/desktop/header1.jpg",
-        mobile: "../img/mobile/header1.jpg"
+        desktop: "../img/desktop/header1.WebP",
+        mobile: "../img/mobile/header1.WebP"
     },
     {
-        desktop: "../img/desktop/header2.jpg",
-        mobile: "../img/mobile/header2.jpg"
+        desktop: "../img/desktop/header2.WebP",
+        mobile: "../img/mobile/header2.WebP"
     },
     {
-        desktop: "../img/desktop/header3.jpg",
-        mobile: "../img/mobile/header3.jpg"
+        desktop: "../img/desktop/header3.WebP",
+        mobile: "../img/mobile/header3.WebP"
+    }];
+    const authorImgContent = [{
+        desktop: "../img/desktop/Marcin-Desktop.WebP",
+        mobile: "../img/mobile/Marcin-Mobile.WebP"
     }];
 
     let active = 0;
-    const time = 8000;
+    const time = 4000;
     const image = document.querySelector('img.main-slide');
     const dots = [...document.querySelectorAll('.dots i')];
     const leftAngle = document.querySelector('.fa-angle-left');
     const rightAngle = document.querySelector('.fa-angle-right');
+    const authorImg = document.querySelector('.author-img');
     const offset = window.innerWidth >= 1024 ? true : false;
 
-    if (offset) image.src = slideList[active].desktop;
-    else image.src = slideList[active].mobile;
+    if (offset) {
+        image.src = slideList[active].desktop;
+        authorImg.src = authorImgContent[0].desktop;
+    } else {
+        image.src = slideList[active].mobile;
+        authorImg.src = authorImgContent[0].mobile;
+    }
 
     const changeDot = () => {
         const activeDot = dots.findIndex(dot => dot.classList.contains('active-dot'));
@@ -32,7 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const autoChangeSlide = () => {
         active++;
         const offset = window.innerWidth >= 1024 ? true : false;
-        console.log(offset);
         if (active === slideList.length) active = 0;
         if (offset) image.src = slideList[active].desktop;
         else image.src = slideList[active].mobile;
@@ -46,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
             duration: 1000,
             iterations: 1
         });
-        changeDot()
+        changeDot();
     }
 
     let sliderInterval = setInterval(autoChangeSlide, time);
@@ -64,5 +73,16 @@ document.addEventListener('DOMContentLoaded', () => {
         window.clearInterval(sliderInterval);
         autoChangeSlide();
         sliderInterval = setInterval(autoChangeSlide, time);
+    });
+    dots.forEach((dot, index) => {
+        index++;
+        dot.addEventListener('click', () => {
+            window.clearInterval(sliderInterval);
+            if (index === 1) active = 2;
+            if (index === 2) active = 0;
+            if (index === 3) active = 1;
+            autoChangeSlide();
+            sliderInterval = setInterval(autoChangeSlide, time);
+        });
     });
 });
